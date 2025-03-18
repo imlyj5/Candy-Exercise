479299f2676ca6663b75c0aa293866d65169a78f
Jeslyn Lai <imlyj5@gmail.com>
2025-03-18 11:34:08 -0700

    finished the get_friends_favorite_candy_count and create_new_candy_data_structure and pass two tests

--- candy_problem/candy_problem.py
+++ candy_problem/candy_problem.py
@@ -1,8 +1,21 @@
 def get_friends_favorite_candy_count(favorites):
-    pass
+    frequency = {}
+    for friend in favorites:
+        for candy in friend[1]:
+            if candy not in frequency:
+                frequency[candy] = 1
+            else:
+                frequency[candy]+=1
+    return frequency
 
 def create_new_candy_data_structure(data):
-    pass 
+    preference = {key: [] for key in get_friends_favorite_candy_count(data).keys()}
+    for friend in data:
+        for candy in friend[1]:
+            if candy in preference:
+                preference[candy].append(friend[0])
+    return preference
+
 
 def get_friends_who_like_specific_candy(data, candy_name):
     pass
@@ -11,3 +24,13 @@ def create_candy_set(data):
     pass 
 
 
+friend_favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+]
+
+favorite_candy_count = get_friends_favorite_candy_count(friend_favorites)
+print(favorite_candy_count)
+print(create_new_candy_data_structure(friend_favorites))
